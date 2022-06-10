67f23739302c83e604383fcf1216cc7d617b9fd1
Theresa <tab61295@gmail.com>
2022-06-10 14:15:17 -0500

    start questions

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const generatePage = require("./src/page-template");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const promptProject = portfolioData => {
+    return inquirer.prompt ([
+    {
+        type: "input",
+        name: "name",
+        message: "What is the name of your project? (Required)",
+        validate: projectNameInput => {
+            if (projectNameInput) {
+                return true;
+            }
+            else {
+                console.log("Please enter the name of your project!");
+                return false;
+            }
+        }
+    }
+])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        }
+        else {
+            return portfolioData;
+        }
+    });
+};
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) { }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() { }
 
 // Function call to initialize app
 init();
